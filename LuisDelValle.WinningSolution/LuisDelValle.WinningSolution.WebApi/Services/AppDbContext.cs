bfde399918a0c88aee2b8f9b02bc68780fd89f1c
luisdvalle <jums.delvalle@gmail.com>
2018-05-29 23:07:04 +1000

    Adding DbContext

---
+++ LuisDelValle.WinningSolution/LuisDelValle.WinningSolution.WebApi/Services/AppDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Blueshift.EntityFrameworkCore.MongoDB.Annotations;
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+
+namespace LuisDelValle.WinningSolution.WebApi.Services
+{
+    [MongoDatabase("appDb")]
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        public AppDbContext()
+            : this(new DbContextOptions<AppDbContext>())
+        {
+        }
+
+        public AppDbContext(DbContextOptions<AppDbContext> appDbContextOptions)
+            : base(appDbContextOptions)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var connectionString = "mongodb://localhost";
+            var mongoUrl = new MongoUrl(connectionString);
+
+            MongoClientSettings settings = MongoClientSettings.FromUrl(mongoUrl);
+
+            MongoClient mongoClient = new MongoClient(settings);
+            options.UseMongoDb(mongoClient);
+        }
+    }
+}
