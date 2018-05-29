4559965b637f59b4c0e9806a5811c928775b3a2a
luisdvalle <jums.delvalle@gmail.com>
2018-05-29 23:30:40 +1000

    Implementing Data Service

---
+++ LuisDelValle.WinningSolution/LuisDelValle.WinningSolution.WebApi/Services/DataService.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LuisDelValle.WinningSolution.WebApi.Services
+{
+    public class DataService<T> : IDataService<T> where T : class
+    {
+        private AppDbContext _dbContext;
+        private DbSet<T> _dbSet;
+
+        public DataService()
+        {
+            _dbContext = new AppDbContext();
+            _dbSet = _dbContext.Set<T>();
+        }
+
+        public void Add(T item)
+        {
+            _dbSet.Add(item);
+            _dbContext.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetSingle(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.SingleOrDefault(predicate);
+        }
+
+        public IEnumerable<T> Query(Expression<Func<T, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+    }
+}
---
+++ LuisDelValle.WinningSolution/LuisDelValle.WinningSolution.WebApi/Services/IDataService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace LuisDelValle.WinningSolution.WebApi.Services
+{
+    public interface IDataService<T>
+    {
+        IEnumerable<T> GetAll();
+        void Add(T item);
+        T GetSingle(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> Query(Expression<Func<T, bool>> predicate);
+    }
+}
