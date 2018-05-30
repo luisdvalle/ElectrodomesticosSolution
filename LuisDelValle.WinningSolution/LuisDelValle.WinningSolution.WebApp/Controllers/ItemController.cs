8c74f7297e06664d91076dfedc31e637f63ad046
luisdvalle <jums.delvalle@gmail.com>
2018-05-31 04:47:05 +1000

    Creating additional controller and view for mvc project

--- LuisDelValle.WinningSolution/LuisDelValle.WinningSolution.WebApp/Controllers/ItemController.cs
+++ LuisDelValle.WinningSolution/LuisDelValle.WinningSolution.WebApp/Controllers/ItemController.cs
@@ -3,24 +3,53 @@ using System.Linq;
 using System.Threading.Tasks;
 using LuisDelValle.WinningSolution.Abstractions;
 using LuisDelValle.WinningSolution.Models;
+using LuisDelValle.WinningSolution.WebApp.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LuisDelValle.WinningSolution.WebApp.Controllers
 {
     public class ItemController : Controller
     {
-        private IRestClient<List<Item>> _restClient;
+        private IRestClient<IEnumerable<Item>> _restClient;
 
-        public ItemController(IRestClient<List<Item>> restClient)
+        public ItemController(IRestClient<IEnumerable<Item>> restClient)
         {
             _restClient = restClient;
+            _restClient.Host = "http://localhost:56346/";
         }
 
-        public async Task<IActionResult> All()
+        [HttpPost]
+        public async Task<IActionResult> GetAll()
         {
+            _restClient.Path = "api/product";
             List<Item> items = await _restClient.GetResponseAsync() as List<Item>;
 
-            return View(items);
+            // mapping to vm.
+            ProductsViewModel vm = new ProductsViewModel
+            {
+                Message = "All products in inventory",
+                NumberProducts = items.Count(),
+                Products = items
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetMinPrice()
+        {
+            _restClient.Path = "api/product/minimumprice";
+            List<Item> items = await _restClient.GetResponseAsync() as List<Item>;
+
+            // mapping to vm.
+            ProductsViewModel vm = new ProductsViewModel
+            {
+                Message = "All products with the minimum price",
+                NumberProducts = items.Count(),
+                Products = items
+            };
+
+            return View(vm);
         }
     }
 }
